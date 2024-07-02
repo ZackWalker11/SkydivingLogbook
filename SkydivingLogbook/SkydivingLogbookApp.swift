bf23797b64fc42c173ae347cb420796c0a34a69e
Zack <Creatureofthemac@gmail.com>
2024-07-02 14:32:15 -0600

    Initial Commit

---
+++ SkydivingLogbook/SkydivingLogbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkydivingLogbookApp.swift
+//  SkydivingLogbook
+//
+//  Created by Zack Walker on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SkydivingLogbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
